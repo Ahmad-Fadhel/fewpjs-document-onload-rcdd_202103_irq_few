b38c9ac6d527ca4628231b19fbc8c4f8721fe616
Ahmad Fadhel <ahmad.f.ameen@gmail.com>
2021-04-05 01:07:37 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // Your code goes here
 document.addEventListener("DOMContentLoaded", function() {
   let textP =document.getElementById("text");
-  textP.textContent = "This is really cool !";
+  textP.textContent = "This is really cool!";
 });
